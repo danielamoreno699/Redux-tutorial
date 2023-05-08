f9bed59325041c1dfbe627f205559d9176702954
Daniela Moreno <dani9292@hotmail.com>
2023-05-08 12:52:16 -0500

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { CartIcon } from "../icons";
+import { useSelector } from "react-redux";
+
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav >
+        <div className="nav-center">
+        <h3>redux toolkit</h3>
+
+        </div>
+    
+    
+    </nav>
+  )
+}
+
+export default Navbar
